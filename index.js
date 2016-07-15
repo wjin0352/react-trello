8ac780f2137998c1ffca039f76bf300fe7590d4e
won <wjin0352@gmail.com>
2016-07-15 19:40:50 -0400

    trello props

--- index.js
+++ index.js
@@ -5,49 +5,19 @@ var ReactDOM = require('react-dom');
 // The Board component should consist of a number of List components,
 // and the List component should contain a number of Cards.
 
-var List = function(props) {
-  return (
-    <div className="list">
-      <li>{props.item}</li>
-    </div>
-  )
-}
 
-var Card = function(props) {
-
-  return (
-    <li>card</li>
-  )
-}
 
 var Board = function(props) {
-  var title = 'first board';
-  var list = [];
-
-  var listArray = props.lists.split(" ");
-  // for(var i = 0; i < listArray.length; i++) {
-  //   list.push(<List item={listArray[i]}/>);
-  // }
-  var results = listArray.map(function(item) {
-    return <li>{item}</li>;
-  })
-  console.log(results[0].props.children);
+  // A title prop which contains the title of the board
+  var title = props.title;
+  // A lists prop which contains an array of the titles of the board's lists
 
   return (
-    <container>
-      <div className="board">
-        {props.title}
-      </div>
-      <div className="lists">
-        <List item={results}/>
-      </div>
-    </container>
-  );
+
+  )
 }
 
-
-
-
+// Test this out by rendering a board which contains multiple lists, and a list which contains multiple cards.
 document.addEventListener('DOMContentLoaded', function() {
-  ReactDOM.render(<Board title="board #1" lists="feature1 feature2 feat3 tests notes" cards="write css, prepare mongo database, setup npm scripts"/>, document.getElementById('app'));
+  ReactDOM.render(<Board title="board#1" getLists="list#1"/>, document.getElementById('app'));
 });
